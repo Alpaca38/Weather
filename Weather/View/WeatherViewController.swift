efc6dfa7a99ad3710a3a0238958f1aa6beb8d9dd
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-07-10 19:20:34 +0900

    날씨 화면 바텀 뷰 구성

--- Weather/View/WeatherViewController.swift
+++ Weather/View/WeatherViewController.swift
@@ -24,6 +24,37 @@ final class WeatherViewController: BaseViewController {
         self.view.addSubview(view)
         return view
     }()
+    private lazy var tableView = {
+        let view = UITableView()
+        view.delegate = self
+        view.dataSource = self
+        self.view.addSubview(view)
+        return view
+    }()
+    private lazy var mapButton = {
+        let view = UIButton()
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .light)
+        view.setImage(UIImage(systemName: "map", withConfiguration: imageConfig), for: .normal)
+        view.tintColor = .white
+        view.addTarget(self, action: #selector(mapButtonTapped), for: .touchUpInside)
+        return view
+    }()
+    private lazy var citySearchButton = {
+        let view = UIButton()
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .light)
+        view.setImage(UIImage(systemName: "list.bullet", withConfiguration: imageConfig), for: .normal)
+        view.tintColor = .white
+        view.addTarget(self, action: #selector(citySearchButtonTapped), for: .touchUpInside)
+        return view
+    }()
+    private lazy var bottomView = {
+        let view = UIView()
+        view.addSubview(mapButton)
+        view.addSubview(citySearchButton)
+        view.backgroundColor = Color.contentBackgroundColor
+        self.view.addSubview(view)
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +66,21 @@ final class WeatherViewController: BaseViewController {
     override func configureLayout() {
         topView.snp.makeConstraints {
             $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        bottomView.snp.makeConstraints {
+            $0.bottom.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(80)
+        }
+        
+        mapButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalToSuperview().offset(20)
+        }
+        
+        citySearchButton.snp.makeConstraints {
+            $0.top.equalTo(mapButton)
+            $0.trailing.equalToSuperview().offset(-20)
         }
     }
 }
@@ -49,4 +95,25 @@ private extension WeatherViewController {
             tempMinMaxLabel.text = $0?.main.tempMinMaxString
         }
     }
+    
+    @objc func mapButtonTapped() {
+        
+    }
+    
+    @objc func citySearchButtonTapped() {
+        
+    }
+}
+
+extension WeatherViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        return cell
+    }
+    
+    
 }
